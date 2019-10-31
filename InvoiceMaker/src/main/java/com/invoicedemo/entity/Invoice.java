2f52756129722f2ea521d8f3f0bc12da101bee51
Erthax <mateuszkochanek01@gmail.com>
2019-10-31 10:26:06 +0100

    #009.Invoice Entity created.

---
+++ InvoiceMaker/src/main/java/com/invoicedemo/entity/Invoice.java
@@ -0,0 +1,50 @@
+package com.invoicedemo.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Invoice implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private int customerId;
+	private ArrayList <InvoiceSubject> invoiceSubjects;
+	
+	public Invoice() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public ArrayList<InvoiceSubject> getInvoiceSubjects() {
+		return invoiceSubjects;
+	}
+
+	public void setInvoiceSubjects(ArrayList<InvoiceSubject> invoiceSubjects) {
+		this.invoiceSubjects = invoiceSubjects;
+	}
+
+	@Override
+	public String toString() {
+		return "Invoice [id=" + id + ", customerId=" + customerId + ", invoiceSubjects=" + invoiceSubjects + "]";
+	}
+	
+	
+}
